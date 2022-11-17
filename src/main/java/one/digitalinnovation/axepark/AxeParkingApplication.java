45a24b4a231868e37d62f2f6e0e618df52d9fb6f
deafpenguin <victormurilo@gmail.com>
2022-11-17 07:54:05 -0300

    Inicialização do projeto

---
+++ src/main/java/one/digitalinnovation/axepark/AxeParkingApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.axepark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxeParkingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxeParkingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/one/digitalinnovation/axepark/AxeParkingApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.axepark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxeParkingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
