6ba8d28207c51fd4ad84362fc3e33905dee98afd
DeafPenguin <victormurilo@gmail.com>
2022-11-26 21:44:56 -0300

    Refatorando testes

---
+++ src/test/java/one/digitalinnovation/axepark/controller/AbstractContainerBase.java
@@ -0,0 +1,20 @@
+package one.digitalinnovation.axepark.controller;
+
+import org.testcontainers.containers.PostgreSQLContainer;
+
+public abstract class AbstractContainerBase {
+
+    static final PostgreSQLContainer POSTGRE_SQL_CONTAINER;
+
+    static {
+        POSTGRE_SQL_CONTAINER = new PostgreSQLContainer("postgres:10-alpine");
+        POSTGRE_SQL_CONTAINER.start();
+
+        System.out.println("********************** " + POSTGRE_SQL_CONTAINER.getJdbcUrl());
+
+        System.setProperty("spring.datasource.url", POSTGRE_SQL_CONTAINER.getJdbcUrl());
+        System.setProperty("spring.datasource.username", POSTGRE_SQL_CONTAINER.getUsername());
+        System.setProperty("spring.datasource.password", POSTGRE_SQL_CONTAINER.getPassword());
+    }
+
+}
--- src/test/java/one/digitalinnovation/axepark/controller/ParkingControllerTest.java
+++ src/test/java/one/digitalinnovation/axepark/controller/ParkingControllerTest.java
@@ -11,7 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-class ParkingControllerTest {
+class ParkingControllerTest extends AbstractContainerBase {
 
     @LocalServerPort
     private int randomPort;
